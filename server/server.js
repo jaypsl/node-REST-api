fa5a91cb1e6722307e1c183b74f177f53b202499
Jaypal Solanki <jaypsl263@gmail.com>
2018-03-30 20:48:04 -0400

    add DELETE /todos/:id route

--- server/server.js
+++ server/server.js
@@ -65,6 +65,31 @@ res.send(doc);
  	})
  })
 
+ app.delete('/todos/:id', (req,res)=> {
+//get the id
+var id = req.params.id;
+
+//validate the id
+if(!ObjectID.isValid(id)){
+	return res.status(404).send();
+}
+
+//remove todo by id
+Todo.findByIdAndRemove(id).then((todo)=>{
+if(!todo){
+	return res.status(404).send();
+}
+res.send(todo);
+}).catch((e)=>{
+	res.status(400).send();
+});
+	//success
+		//if no doc,send 404
+		// if doc, send 200
+	//error
+		//400 with empty body
+ });
+
 app.listen(port, ()=> {
 	console.log(`started on port ${port}`);
 })
